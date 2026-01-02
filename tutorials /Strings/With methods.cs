932cbd2a5255c8feb7596baf1e13011f4b2c6d5d
Bee <225415206+blackphoenix23@users.noreply.github.com>
2026-01-01 19:20:33 -0800

    Add files via upload

---
+++ tutorials /Strings/With methods.cs
@@ -0,0 +1,9 @@
+
+
+string songLyrics = "You say goodbye, and I say hello"; 
+Console.WriteLine(songLyrics.StartsWith("You")); 
+Console.WriteLine(songLyrics.StartsWith("goodbye")); 
+//StartsWith: displays the boolean value of string (if that word in the string is in the start)
+Console.WriteLine(songLyrics.EndsWith("hello")); 
+Console.WriteLine(songLyrics.EndsWith("goodbye"));
+//EndsWith: displays the boolean value of string (if that word in the string in in the end)
